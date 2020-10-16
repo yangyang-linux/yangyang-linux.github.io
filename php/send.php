6843e3d9fcb6a54ebaea3dddbd74d79eeb43e7fd
yangyang <57141784+yangyang-linux@users.noreply.github.com>
2020-10-16 18:27:45 +0800

    Update send.php

--- php/send.php
+++ php/send.php
@@ -5,19 +5,18 @@ $mail = new PHPMailer();
 $mail->Encoding = "base64";
 $mail->isSMTP(); 					// 使用SMTP服务
 $mail->CharSet = "GB2312";			//应该使用"UTF-8"编码，但邮件收到乱码，所以使用GB2312;
-$mail->Host = "smtp.163.com";      	// 发送方的SMTP服务器地址
+$mail->Host = "smtp.qq.com";      	// 发送方的SMTP服务器地址
 $mail->SMTPAuth = true;				// 是否使用身份验证
-$mail->Username = "****@163.com";   // 发送方的163邮箱用户名
+$mail->Username = "****@qq.com";   // 发送方的qq邮箱用户名
 $mail->Password = "yourpassword"; 	// 发送方的邮箱授权码
 $mail->SMTPSecure = "ssl";			// 使用ssl协议方式
 $mail->Port = 465;   				// 163邮箱的ssl协议方式端口号是465/994
 //$mail->isHTML(true); 
 
-$mail->setFrom("****@163.com","邮件主题");     // 设置发件人信息
-$mail->addAddress("收件人1@qq.com",""); // 设置收件人信息，可设置多个，复制粘贴此行修改邮箱地址即可
-$mail->addAddress("收件人2@**.com","");  // 设置收件人信息，可设置多个，复制粘贴此行修改邮箱地址即可
-$mail->addAddress("**@**.com",""); 	// 设置收件人信息，可设置多个，复制粘贴此行修改邮箱地址即可
-$mail->addReplyTo("***@163.com","Reply");   // 设置回复人信息，指的是收件人收到邮件后，如果要回复，回复邮件将发送到的邮箱地址
+$mail->setFrom("****@q'q.com","邮件主题");     // 设置发件人信息
+$mail->addAddress("2985409357@qq.com",""); // 设置收件人信息，可设置多个，复制粘贴此行修改邮箱地址即可
+
+$mail->addReplyTo("***@qq.com","Reply");   // 设置回复人信息，指的是收件人收到邮件后，如果要回复，回复邮件将发送到的邮箱地址
 //$mail->addCC("evolraelc9@163.com");    // 设置邮件抄送人，可以只写地址，上述的设置也可以只写地址
 //$mail->addBCC("bbbb@163.com");         // 设置秘密抄送人
 //$mail->addAttachment("bug0.jpg");      // 添加附件
@@ -31,9 +30,6 @@ $email2 = iconv('UTF-8','GB2312',$emailtemp);
 
 $phoneltemp = $_POST['phone'];
 $phone2 = iconv('UTF-8','GB2312',$phoneltemp);
-
-$bookSelecttemp = $_POST['bookSelect'];
-$bookSelect2 = iconv('UTF-8','GB2312',$bookSelecttemp);
 
 $commentstemp = $_POST['comments'];
 $comments2 = iconv('UTF-8','GB2312',$commentstemp);
